3a1f807afed8c51e4950746c636533d9752ae8f3
Devin <devin.p.flood@gmail.com>
2018-09-10 15:22:22 -0700

    Finish first Routes and Controllers

--- FirstRoutes/app/controllers/users_controller.rb
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -4,11 +4,42 @@ class UsersController < ApplicationController
   end
   
   def create
-    render json: params
+    user = User.new(user_params)
+    
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
   end
   
   def show
-    render json: params
+    user = User.find(params[:id])
+    render json: user
   end
   
+  def update 
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    if user.delete
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+  
+  private 
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
